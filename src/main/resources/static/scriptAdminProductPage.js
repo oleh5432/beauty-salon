fc415dacbb30373abab9cadd7946e72fcb5cec88
Oleh <oleh5432@gmail.com>
2019-04-13 22:07:37 +0300

    Added the option to select a category when upgraded

--- src/main/resources/static/scriptAdminProductPage.js
+++ src/main/resources/static/scriptAdminProductPage.js
@@ -4,7 +4,7 @@ getAllProducts();
 
 setModalConfiguration();
 setModalCategoryConfiguration();
-// setModalUpdateConfiguration();
+setModalUpdateConfiguration();
 
 setActionOnUpdateProductButtons();
 setActionOnUpdateButton();
@@ -162,7 +162,6 @@ function getAllCategories() {
         success: function (dataResponse) {
             setCategoriesToTable(dataResponse.data);
             setActionOnSelectCategoryButtons();
-            // setActionOnDeleteButtons();
         },
         error: function (error) {
             console.log(error.message);
@@ -190,7 +189,13 @@ function setActionOnSelectCategoryButtons() {
     $(".button").each(function (index) {
         $(this).click(function () {
             $('#categoryId').val($(this).val());
+            $('#categoryIdUpdate').val($(this).val());
             document.getElementById('modalCategory').style.display = "none";
+            if (!(document.getElementById('myModal').style.display === "block")) {
+                if(document.getElementById('modalUpdate').style.display === "none"){
+                    document.getElementById('modalUpdate').style.display = "block";
+                }
+            }
         })
     })
 }
@@ -203,7 +208,7 @@ function setModalConfiguration() {
     var btn = document.getElementById("openModal");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementById("closeModal");
 
     // When the user clicks the button, open the modal
     btn.onclick = function () {
@@ -225,19 +230,55 @@ function setModalConfiguration() {
 
 function setModalCategoryConfiguration() {
     // Get the modal
-    var modal = document.getElementById('modalCategory');
+    var modalCategory = document.getElementById('modalCategory');
+    var modalUpdate = document.getElementById('modalUpdate');
 
     // Get the button that opens the modal
     var btn = document.getElementById("selectCategory");
+    var btn2 = document.getElementById("selectCategoryUpdate");
 
     // Get the <span> element that closes the modal
     var span = document.getElementById("closeModalCategory");
 
     // When the user clicks the button, open the modal
     btn.onclick = function () {
-        modal.style.display = "block";
+        modalCategory.style.display = "block";
         getAllCategories();
     };
+    btn2.onclick = function () {
+        modalCategory.style.display = "block";
+        modalUpdate.style.display = "none";
+        getAllCategories();
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modalCategory.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+}
+
+//конфіг для модального вікна оновлення
+function setModalUpdateConfiguration() {
+    // Get the modal
+    var modal = document.getElementById('modalUpdate');
+
+    // Get the button that opens the modal
+    // var btn = document.getElementById("openModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementById('closeModalUpdate');
+
+    // When the user clicks the button, open the modal
+    // btn.onclick = function () {
+    //     modal.style.display = "block";
+    // };
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
@@ -251,5 +292,3 @@ function setModalCategoryConfiguration() {
         }
     };
 }
-
-//конфіг для модального вікна оновлення
