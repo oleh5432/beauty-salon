2c2c6a26468e7762ff23939b72ba55fffebf0bcb
Oleh <oleh5432@gmail.com>
2019-05-01 11:09:13 +0300

    Added FileController

---
+++ src/main/java/kurakh/beautysalon/controller/FileController.java
@@ -0,0 +1,25 @@
+package kurakh.beautysalon.controller;
+
+import kurakh.beautysalon.dto.request.FileRequest;
+import kurakh.beautysalon.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@CrossOrigin
+@RestController
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping("/upload")
+    public String upload(@RequestBody FileRequest fileRequest) throws IOException {
+        String filePath = fileService.saveFile(fileRequest);
+        return filePath;
+    }
+}
